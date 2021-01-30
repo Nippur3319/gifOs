7b148fc705d68620b22d3dbc508d2cfecf13fac5
Nippur3319 <nicolasfoppiani@gmail.com>
2021-01-30 14:05:49 -0300

    layout y logica de trending completa para mobile

--- js/trending.js
+++ js/trending.js
@@ -1,10 +1,11 @@
 let apiKey = "SLANiqAne2Dq6vVtpS4j9ZOSnERNk2Ly"
 let trendingDinamicContainer = document.getElementById("trending-dinamic-container");
 
+let trendingTermsTxt = document.getElementById("trending_terms_txt");
 
 // Muestra por pantalla los GIF trending del momento con el endpoint de Giphy
 const getGiphyTrendings = async () => {
-    let url = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=15&rating=g`;
+    let url = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=5&rating=g`;
     const resp = await fetch(url);
     const giphyTrendings = await resp.json();
     console.log(giphyTrendings);
@@ -17,7 +18,11 @@ function addToDom (giphyResults) {
     
 
     for (let i = 0; i < giphyResults.data.length; i++) {
+        //tratando de recortar el titulo hasta GIF
         
+        let titulo = giphyResults.data[i].title
+        
+        trendingTermsTxt.innerHTML += cortarTitulo(titulo) + " - "
         
         let gif = document.createElement("img");
         gif.className = "trending-gif-img"
@@ -27,6 +32,14 @@ function addToDom (giphyResults) {
     }
 
 }
+
+// extraigo el título del gif hasta la encontrar la cadena "GIF"
+function cortarTitulo (titulo) {
+    let indiceHastaGet = titulo.search("GIF")
+    let tituloCortado =  titulo.slice(0,indiceHastaGet)
+    return tituloCortado
+}
+
 
 // Se llama a getGiphyTrendings() al cargar la pagina
 window.onload = () => {
